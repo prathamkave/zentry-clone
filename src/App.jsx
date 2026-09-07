10c3866c7ead50b61aa0baef1d7a1e37bd6982a5
Pratham Kave <kavepratham@gmail.com>
2026-09-07 19:10:15 +0530

    Story,Feature,Contact & Footer Component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-const App = () => {
-	return <div className=''>App</div>;
-};
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
 
 export default App;
